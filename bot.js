04936fb8da490b5fbfce809f39419a20e62c945d
Leo. <erenesaslan@hotmail.com>
2024-08-16 11:11:48 +0300

    Design changes
    Update our code to the new version

--- bot.js
+++ bot.js
@@ -1,77 +1,39 @@
-require('dotenv').config();
-const {Client, Events, GatewayIntentBits} = require('discord.js')
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers,
-    ]
-});
-const PREFIX = '31 '
+const fs = require('node:fs');
+const path = require('node:path');
+const {Client, Collection, GatewayIntentBits} = require('discord.js');
+const {token} = require('./config.json');
 
-client.once(Events.ClientReady, readyClient => {
-    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
 
-client.on('messageCreate', (message) => {
-    if (message.content.startsWith(PREFIX)) {
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
-        const [CMD_NAME, ...args] = message.content
-            .trim()
-            .substring(PREFIX.length)
-            .split(' ');
-
-        if (CMD_NAME === 'kick') {
-
-            if (args[0].toString().startsWith('<@')) {
-
-                if (message.member.permissions.has('2')) {
-
-                    message.guild.members.kick(args[0]
-                        .toString()
-                        .slice(2, 20))
-                        .then(
-                            message.reply('user kicked!')
-                        )
-                        .catch(err => console.log(err));
-                } else {
-                    message.reply('Yapamazsin orospu evladi')
-                }
-
-            }
-
-
-        } else if (CMD_NAME === 'ban') {
-
-            if (args[0].toString().startsWith('<@')) {
-
-                if (message.member.permissions.has('4')) {
-
-                    message.guild.members.kick(args[0]
-                        .toString()
-                        .slice(2, 20))
-                        .then(
-                            message.reply('user kicked!')
-                        )
-                        .catch(err => console.log(err));
-                } else {
-                    message.reply('Yapamazsin orospu evladi')
-                }
-
-            }
-
-        } else if (CMD_NAME === 'delete') {
-
-            if (message.member.permissions.has('8192')){
-                message.channel
-            }
-
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
         }
-
-
     }
+}
 
-});
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-client.login(process.env.DISCORDJS_BOT_TOKEN);
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
+client.login(token);
